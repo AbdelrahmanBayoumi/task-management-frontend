8d0725ae96d9fb060dd1327db03dd47712f9d623
AbdelrahmanBayoumi <AbdelrahmanBayoumi@users.noreply.github.com>
2024-01-22 14:57:29 +0000

    deploy: fba62ff27247febb7525e7b474de9ad0ef2b2e9d

---
+++ precache-manifest.69f96b3fbc7666ce57e4062bf3559b99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805c6f6dd584060e7d25c25d476569e3",
+    "url": "https://abdelrahmanbayoumi.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "7abb115a3afa71758065",
+    "url": "https://abdelrahmanbayoumi.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "c06e17b8750f8912a38e",
+    "url": "https://abdelrahmanbayoumi.github.io/task-management-frontend/static/js/2.cd88b106.chunk.js"
+  },
+  {
+    "revision": "7abb115a3afa71758065",
+    "url": "https://abdelrahmanbayoumi.github.io/task-management-frontend/static/js/main.83795507.chunk.js"
+  },
+  {
+    "revision": "d47cf1f85f1e871ca000",
+    "url": "https://abdelrahmanbayoumi.github.io/task-management-frontend/static/js/runtime~main.84a29ad9.js"
+  }
+]);
